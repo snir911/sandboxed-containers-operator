0fc6655c5ce5c9831b6ca273368c43a6cd9d9eef
Snir Sheriber <ssheribe@redhat.com>
2025-03-11 10:40:33 +0200

    inject short gpu attestation logs

--- config/peerpods/podvm/bootc/nvidia/gpu-local-attestation.sh
+++ config/peerpods/podvm/bootc/nvidia/gpu-local-attestation.sh
@@ -3,5 +3,6 @@
 # gpu attestation
 if [[ " $@ " =~ " prestart " ]]; then
   python3 -m verifier.cc_admin  > /var/log/gpu-attestation-status 2>&1
+  tail -3 /var/log/gpu-attestation-status > /var/log/gpu-attestation-status-short
   nohup /usr/local/bin/log-inject.sh & # this passes file to containers
 fi
