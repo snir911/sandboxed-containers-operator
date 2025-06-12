da451d26eb04935f48aa86ed5b6e47cc3b849d4d
Snir Sheriber <ssheribe@redhat.com>
2025-06-12 16:43:05 +0300

    aws: rbac rules required for prebuilt artifacts upload
    by the AWS podvm image creation scripts

---
+++ config/peerpods/podvm/extra_rbac.go
@@ -0,0 +1,4 @@
+package rbac
+
+// the following is required by AWS podvm creation scripts
+//+kubebuilder:rbac:groups=cloudcredential.openshift.io,resources=credentialsrequests,verbs=create;delete;get;list
